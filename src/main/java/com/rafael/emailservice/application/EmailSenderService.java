c8340ec7793f4895c90ed970ce5bdb8c62bbc083
Rafael Almeida <rafaelalmeidasa@gmail.com>
2023-10-04 17:31:25 -0300

    feat: create application

---
+++ src/main/java/com/rafael/emailservice/application/EmailSenderService.java
@@ -0,0 +1,20 @@
+package com.rafael.emailservice.application;
+
+import com.rafael.emailservice.adapters.EmailSenderGateway;
+import com.rafael.emailservice.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
